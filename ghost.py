c80b817f6b19a16a1c8a66ce552d3aa54e1b9e2b
David Ginzbourg <davidginzbourg@gmail.com>
2016-12-14 00:17:50 +0200

    Add stud init

--- ghost.py
+++ ghost.py
@@ -775,7 +775,13 @@ class S3Storage(object):
 
     def init(self):
         # init stuff
-        self.client.create_bucket(Bucket=self.bucket_name, CreateBucketConfiguration=self.bucket_configuration)
+        # TODO: Check best object configurations for the init
+        # self.client.create_bucket(Bucket=self.bucket_name, CreateBucketConfiguration=self.bucket_configuration)
+        self.client.put_object(
+            Body='Empty Ghost stash',
+            Bucket = self.bucket_name,
+            Key='Ghost'
+        )
 
     def put(self, key):
         """Insert the key and return its database id
